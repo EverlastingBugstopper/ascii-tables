758a7515e396abae573677a453923dc702cfd20f
Avery Harnish <averyharnish@gmail.com>
2019-11-15 14:55:32 -0600

    Adds Rust to comment style dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,7 +118,7 @@ function createTable() {
     case "none":
         break;
     case "doubleslant":
-        // C++/C#/F#/Java/JavaScript/Swift
+        // C++/C#/F#/Java/JavaScript/Swift/Rust
         prefix = "// ";
         break;
     case "hash":
